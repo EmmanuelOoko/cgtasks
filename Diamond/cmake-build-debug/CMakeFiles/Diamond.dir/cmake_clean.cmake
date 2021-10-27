6fc95544ffe25cdb94563740416d60bcb247edc1
EmmanuelOoko <emmanuel.ooko@strathmore.edu>
2021-10-27 13:32:40 +0300

    to upload diamond

---
+++ Diamond/cmake-build-debug/CMakeFiles/Diamond.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Diamond.dir/main.cpp.o"
+  "Diamond"
+  "Diamond.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Diamond.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
